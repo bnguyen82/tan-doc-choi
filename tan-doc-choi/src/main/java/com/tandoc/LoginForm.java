f1f1fdfba7cad600b7d3f28cb8557b70a90dbadc
avaxnguyen <avaxnguyen@5820c510-f059-11de-bf31-4dec8810c1c1>
2009-12-28 08:29:39 +0000

    Set validate=false Run first action home.do

--- tan-doc-choi/src/main/java/com/tandoc/LoginForm.java
+++ tan-doc-choi/src/main/java/com/tandoc/LoginForm.java
@@ -1,5 +1,18 @@
 package com.tandoc;
 
-public class LoginForm {
+import java.io.Serializable;
 
+import org.apache.struts.validator.ValidatorActionForm;
+import org.apache.struts.action.ActionServlet;
+
+
+public class LoginForm extends ValidatorActionForm implements Serializable{
+	private String username;
+	 public String getUsername() {
+		   return username;
+		 }
+	 public void setUsername(String username) {
+		   this.username = username;
+		 }
+	
 }
--- tan-doc-choi/src/main/java/com/tandoc/actions/HomeAction.java
+++ tan-doc-choi/src/main/java/com/tandoc/actions/HomeAction.java
@@ -1,5 +1,22 @@
 package com.tandoc.actions;
 
-public class HomeAction {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+public class HomeAction extends Action {
+	  public ActionForward execute(ActionMapping mapping,
+              ActionForm form,
+              HttpServletRequest request,
+              HttpServletResponse response)
+              throws Exception
+		{		
+		return mapping.findForward("failure");
+		}
+
 
 }
