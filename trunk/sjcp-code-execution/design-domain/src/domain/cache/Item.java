be65f59c8f05535c4d5af2ba9455d51eecc7f073
avaxnguyen@gmail.com <avaxnguyen@gmail.com@5820c510-f059-11de-bf31-4dec8810c1c1>
2012-02-26 15:48:26 +0000

    cache implement divide to add more projectproject

---
+++ trunk/sjcp-code-execution/design-domain/src/domain/cache/Item.java
@@ -0,0 +1,27 @@
+package domain.cache;
+
+public interface Item {
+	public void setCode(String code);
+
+	public int getId();
+
+	public void setId(int id);
+
+	public String getName();
+
+	public void setName(String name);
+
+	public String getPrice();
+
+	public void setPrice(String price);
+
+	public String getProvider();
+
+	public void setProvider(String provider);
+
+	public String getCountry();
+
+	public void setCountry(String country);
+
+	public String getCode();
+}
---
+++ trunk/sjcp-code-execution/design-domain/src/domain/cache/impl/Clothe.java
@@ -0,0 +1,60 @@
+package domain.cache.impl;
+
+import domain.cache.Item;
+
+public class Clothe implements Item {
+	private int id;
+	private String code;
+	private String name;
+	private String price;
+	private String provider;
+	private String country;
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	public String getProvider() {
+		return provider;
+	}
+
+	public void setProvider(String provider) {
+		this.provider = provider;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getCode() {
+		return code;
+	}
+}
