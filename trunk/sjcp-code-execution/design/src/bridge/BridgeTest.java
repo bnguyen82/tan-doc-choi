d35ee7bc7f839e14adaa8ab0bc32abaa5cf1c542
avaxnguyen@gmail.com <avaxnguyen@gmail.com@5820c510-f059-11de-bf31-4dec8810c1c1>
2012-02-28 15:27:15 +0000

    before apply bridge

---
+++ trunk/sjcp-code-execution/design/src/bridge/BridgeTest.java
@@ -0,0 +1,11 @@
+package bridge;
+
+
+public class BridgeTest {
+	public static void main(String[] args) throws Exception{
+		String text = "Bao";
+		MessageLogger log = new FileLogger();
+		log.log(text);
+			
+	}
+}
