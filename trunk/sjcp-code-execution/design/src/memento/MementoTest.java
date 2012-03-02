8027f728f3678362e158952de90452f3d8d8a837
avaxnguyen@gmail.com <avaxnguyen@gmail.com@5820c510-f059-11de-bf31-4dec8810c1c1>
2012-03-02 14:33:54 +0000

    aggregate added

--- trunk/sjcp-code-execution/design/src/memento/MementoTest.java
+++ trunk/sjcp-code-execution/design/src/memento/MementoTest.java
@@ -2,18 +2,21 @@ package memento;
 
 public class MementoTest {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		StatusHandler handler = new StatusHandler();
-		DataProcess process = new DataProcess(handler.getMemento());
+		DataProcess process  = null;
 		try {
+			process = new DataProcess(handler.getMemento());
+		
 			process.process("memento.txt");
 		}
 		catch(Exception e){
+			try {
 			handler.saveMemento(process.getMemento());
+			}catch(Exception e1){}
+		}
+		finally{
+			
 		}
 	}
-
 }
