78c898c2718637f984a85779a61602e47b6c84c0
avaxnguyen@gmail.com <avaxnguyen@gmail.com@5820c510-f059-11de-bf31-4dec8810c1c1>
2012-07-10 10:25:36 +0000

    Submit advanced-java

---
+++ sjcp-code-execution/advanced-java/src/bao/reflection/ClassA.java
@@ -0,0 +1,8 @@
+package bao.reflection;
+
+public class ClassA {
+	private String a = "Bao";
+	private String b = "Ha";
+	public String c = "Ha";
+		
+}
---
+++ sjcp-code-execution/advanced-java/src/bao/security/classloader/JavaAPILoader.java
@@ -0,0 +1,14 @@
+package bao.security.classloader;
+
+public class JavaAPILoader {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Bao bao = new Bao();
+		System.out.print(bao.toString());
+	}
+
+}
