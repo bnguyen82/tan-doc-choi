8f6f6d58b35bf0e8e2bbbfae0b64bb4cd6f0f77a
avaxnguyen@gmail.com <avaxnguyen@gmail.com@5820c510-f059-11de-bf31-4dec8810c1c1>
2013-06-24 07:14:07 +0000

    Add closure

--- trunk/sjcp-code-execution/angularJSProj/angular/src/main/webapp/normalJS/listener.js
+++ trunk/sjcp-code-execution/angularJSProj/angular/src/main/webapp/normalJS/listener.js
@@ -11,4 +11,40 @@ document.querySelector('body').addEventListener('click', function(event) {
     alert(event.target.id);
   }
 });
+
+//Clousure
+var ob = (function(){
+	var name = 'Bao';
+	var fnShow = function(){
+		return name;
+	};
+	return {
+		ten : 'Good',
+		show : fnShow,
+		edit : function() {
+			name = 'Ha';
+		}
+	};
+})();
+
+var name = ob.show();
+ob.edit();
+var name2 = ob.show();
+
+//New object anonymos
+var ob1 = new function(){
+	var name = 'Bao';
+	var fnShow = function(){
+		return name;
+	};
+	this.ten = 'Good';
+	this.show = fnShow;
+	this.edit = function() {
+			name = 'Ha';
+	};
+}();
+
+var name = ob1.show();
+ob1.edit();
+var name2 = ob1.show();
   
