5d161b7eaeca5f76389b8a479beeda52296bbcfc
avaxnguyen@gmail.com <avaxnguyen@gmail.com@5820c510-f059-11de-bf31-4dec8810c1c1>
2013-07-11 04:05:00 +0000

    Implement CORS successfully

--- trunk/sjcp-code-execution/Spring3MVC-REST/src/main/java/com/mkyong/common/controller/MovieController.java
+++ trunk/sjcp-code-execution/Spring3MVC-REST/src/main/java/com/mkyong/common/controller/MovieController.java
@@ -1,21 +1,34 @@
 package com.mkyong.common.controller;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.mkyong.common.Shop;
 
 @Controller
 @RequestMapping("/movie")
 public class MovieController {
 
-	@RequestMapping(value="/{name}", method = RequestMethod.GET)
-	public String getMovie(@PathVariable String name, ModelMap model) {
+	@RequestMapping(value="/{name}", method = RequestMethod.POST)
+	@ResponseBody
+	public ResponseEntity<Shop> getMovie(@PathVariable String name, ModelMap model) {
 
-		model.addAttribute("movie", name);
-		return "list";
-
+		Shop shop = new Shop();
+		shop.setName(name);
+		shop.setStaffName(new String[]{"mkyong1", "mkyong2"});
+		
+		
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Access-Control-Allow-Origin", "*");
+        ResponseEntity<Shop> entity = new ResponseEntity<Shop>(shop, headers, HttpStatus.CREATED);
+        return entity;
 	}
 	
 }
---
+++ trunk/sjcp-code-execution/Spring3MVC-REST/src/main/webapp/WEB-INF/pages/home.jsp
@@ -0,0 +1,16 @@
+<html >
+
+  <head>
+    <script data-require="angular.js@1.1.5" data-semver="1.1.5" src="http://code.angularjs.org/1.1.5/angular.min.js"></script>
+	 <script data-require="jquery@2.0.1" data-semver="2.0.1" src="http://code.jquery.com/jquery-2.0.1.min.js"></script>
+	<script src="js/jQuery.js"></script>
+  </head>
+  
+	
+  <body>
+  <div > Good Morning
+  </div>      
+  </body>
+
+</html>
+
