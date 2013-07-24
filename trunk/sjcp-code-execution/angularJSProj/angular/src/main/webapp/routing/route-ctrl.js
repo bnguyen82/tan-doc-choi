ffc67308402e3fae51591ca1e53897f397bccc05
avaxnguyen@gmail.com <avaxnguyen@gmail.com@5820c510-f059-11de-bf31-4dec8810c1c1>
2013-07-24 09:29:50 +0000

    Example for ui-router -> restruct html

--- trunk/sjcp-code-execution/angularJSProj/angular/src/main/webapp/routing/route-ctrl.js
+++ trunk/sjcp-code-execution/angularJSProj/angular/src/main/webapp/routing/route-ctrl.js
@@ -1,39 +1,45 @@
-    var myapp = angular.module('myapp', ["ui.state"])
-    myapp.config(function($stateProvider, $urlRouterProvider){
-      
-      // For any unmatched url, send to /route1
-      $urlRouterProvider.otherwise("/home")
-      
-      $stateProvider
-        .state('route1', {
-            url: "/route1",
-            templateUrl: "routing/route1.html"
-        })
-	      .state('route1.list', {
-	          url: "/list",
-	          templateUrl: "routing/route1-list.html",
-	          controller: function($scope){
-	            $scope.items = ["A", "List", "Of", "Items"];
-	          }
-	      })
-          
-        .state('route2', {
-            url: "/route2",
-            templateUrl: "routing/route2.html"
-        })
-          .state('route2.list', {
-              url: "/list",
-              templateUrl: "routing/route2-list.html",
-              controller: function($scope){
-                $scope.things = ["A", "Set", "Of", "Things"];
-              }
-          })
-        .state('home', {
-            url: "/home",
-            template: "Hello World!",
-            controller: function(){
-        		console.log("Enter Home Controller");
-        	}
-        
-        })
+var myapp = angular.module('myapp', ["ui.state"])
+myapp.config(function($stateProvider, $urlRouterProvider){
+  
+  // For any unmatched url, send to /route1
+  $urlRouterProvider.otherwise("/home")
+  
+  $stateProvider
+    .state('route1', {
+        url: "/route1",
+        templateUrl: "routing/views/route1.html"
     })
+      .state('route1.list', {
+          url: "/list",
+          templateUrl: "routing/views/route1-list.html",
+          controller: function($scope){
+            $scope.items = ["A", "List", "Of", "Items"];
+          }
+      })
+      
+    .state('route2', {
+        url: "/route2",
+        templateUrl: "routing/views/route2.html"
+    })
+      .state('route2.list', {
+          url: "/list",
+          templateUrl: "routing/views/route2-list.html",
+          controller: function($scope){
+            $scope.things = ["A", "Set", "Of", "Things"];
+          }
+      })
+    .state('home', {
+        url: "/home",
+        template: "Hello World!",
+        controller: function(){
+    		console.log("Enter Home Controller");
+    	}
+    
+    })
+});
+
+function MainCtrl($state){
+//  $state.transitionTo('contacts.list');
+	console.log("Enter Main Controller");
+}
+    
