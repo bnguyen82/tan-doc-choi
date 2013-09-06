5fd58f7c2c4e1a103fc5049f5d109e4db4461a99
avaxnguyen@gmail.com <avaxnguyen@gmail.com@5820c510-f059-11de-bf31-4dec8810c1c1>
2013-09-06 13:52:09 +0000

    Basic Lucence

---
+++ sjcp-code-execution/basiclucene/src/main/java/com/mylucene/basiclucene/App.java
@@ -0,0 +1,13 @@
+package com.mylucene.basiclucene;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
